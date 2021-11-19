a5bf7dd7711388afe1544edd3e2ae8e5bbae0c1a
Navin136 <nkwhitehat@gmail.com>
2021-11-19 17:27:43 +0530

    KeepEvolving: Love from TamilNadu, India
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

---
+++ KeepEvolving/Navin136/KeepEvolving.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	int i;
+	for(i=0;i<=1000000;i++)
+	{
+		printf("தொடர்ந்து வளர்ந்து கொண்டே இருங்கள்- With love from TamilNadu, India\n");
+	}
+	return 0;
+}
---
+++ KeepEvolving/Navin136/KeepEvolving.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for ((i=0; i<=100000; i++))
+do
+  echo "தொடர்ந்து வளர்ந்து கொண்டே இருங்கள்- With love from TamilNadu, India"
+done
