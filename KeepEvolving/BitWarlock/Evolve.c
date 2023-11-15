d17b0476077b68c66896ccc5754b5548ee829696
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-11-15 19:06:29 +0000

    Love from Sale, Morocco ❤

---
+++ KeepEvolving/BitWarlock/Evolve.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\x1b[31m\x1b[1m");
+    printf("=============================================\n");
+    printf("\t\tKeepEvolving\n");
+    printf("=============================================\n\x1b[0m");
+    return (0);
+}
---
+++ KeepEvolving/BitWarlock/Wisdom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run ./Dragon.sh To Hear the Dragon's wisdom.
+
+echo " _______________________"
+echo "< Hello, KeepEvolving!  >"
+echo " -----------------------"
+echo "   \\"
+echo "    \\"
+echo "        .--."
+echo "       |o_o |"
+echo "       |:_/ |"
+echo "      //   \\ \\"
+echo "     (|     | )"
+echo "    /'\\_   _/\\"
+echo "    \\___)=(___/\\"
